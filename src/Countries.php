7ba2dd2d16a6cce158d87a131427bf5ce14c554f
magkopian <m.agkopian@gmail.com>
2014-10-31 13:53:21 +0200

    Added author in src/Countries.php

--- src/Countries.php
+++ src/Countries.php
@@ -8,6 +8,7 @@ namespace JeroenDesloovere\Countries;
  * Get all the countries in the world and their languages.
  *
  * @author Jeroen Desloovere <info@jeroendesloovere.be>
+ * @author Manolis Agkopian <m.agkopian@gmail.com>
  */
 class Countries
 {
